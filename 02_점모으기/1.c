039add84fe79668cdb3eb5f17b64a8637cc20b05
Donghoon Lee <donghuna@naver.com>
2022-08-22 19:28:53 +0900

    Update 1.c

--- 02_점모으기/1.c
+++ 02_점모으기/1.c
@@ -14,13 +14,13 @@ int rowCnt[LM], colCnt[LM];
 
 int getMedian(int*ap, int tg) {
     for (int i = 1; i < LM; ++i) {
-        tg -= ap[i];
+        tg -= ap[i];                    // 중앙값의 위치를 찾기 위해 개수만큼 빼준다.
         if (tg <= 0) return i;
     }
     return 0;  // impossible
 }
 
-int abs(int a) { return a < 0 ? -a : a; }
+int abs(int a) { return a < 0 ? -a : a; }       // abs
 int main() {
 #ifdef _WIN32
     freopen("02.txt", "r", stdin);
@@ -29,15 +29,15 @@ int main() {
     int r, c, i;
     for (i = 0; i < M; ++i) {
         scanf("%d %d", &r, &c);
-        rowCnt[r] ++;
+        rowCnt[r] ++;               // Row, Col 위치값을 저장한다.
         colCnt[c] ++;
     }
 
-    int mr = getMedian(rowCnt, (M + 1) / 2);
+    int mr = getMedian(rowCnt, (M + 1) / 2);    // 중앙값이 있는 index를 가져온다.
     int mc = getMedian(colCnt, (M + 1) / 2);
 
     for (int i = 0; i < LM; ++i) {
-        ans += rowCnt[i] * abs(mr - i);
+        ans += rowCnt[i] * abs(mr - i);     // 이동거리를 계산한다.
         ans += colCnt[i] * abs(mc - i);
     }
     printf("%d\n", ans);
