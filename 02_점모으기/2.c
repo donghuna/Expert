087722f0cbe59b354fe760a1e484c625b277dc7d
Donghoon Lee <donghuna@naver.com>
2022-08-22 13:48:18 +0900

    Create 2.c

---
+++ 02_점모으기/2.c
@@ -0,0 +1,55 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+/*
+02_점모으기_ver02
+merge sort
+median
+*/
+#include <cstdio>
+
+const int LM = 100004;
+int N, M, ans;
+int row[LM], col[LM], trr[LM];
+
+void mergeSort(int*arr, int s, int e) {
+    // 1. base condition
+    if (s >= e) return;
+    // 2. divide and conquer
+    int m = (s + e) / 2, i = s, j = m + 1, k = s;
+    mergeSort(arr, s, m);
+    mergeSort(arr, m + 1, e);
+
+    // 3. merge
+    while (i <= m && j <= e) {
+        if (arr[i] <= arr[j]) trr[k++] = arr[i++];
+        else trr[k++] = arr[j++];
+    }
+    while (i <= m) trr[k++] = arr[i++];
+    while (j <= e) trr[k++] = arr[j++];
+    // 4. copy
+    for (int i = s; i <= e; ++i) arr[i] = trr[i];
+}
+
+int abs(int a) { return a < 0 ? -a : a; }
+
+int main() {
+#ifdef _WIN32
+    freopen("input.txt", "r", stdin);
+#endif // _WIN32
+    scanf("%d %d", &N, &M);
+    for (int i = 1; i <= M; ++i) {
+        scanf("%d %d", row + i, col + i);
+    }
+    mergeSort(row, 1, M);
+    mergeSort(col, 1, M);
+
+    int mr = row[(M + 1) / 2];
+    int mc = col[(M + 1) / 2];
+    for (int i = 1; i <= M; ++i) {
+        ans += abs(row[i] - mr);
+        ans += abs(col[i] - mc);
+    }
+    printf("%d\n", ans);
+    return 0;
+}
